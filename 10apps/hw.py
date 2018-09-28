617fc8daa73b8f48f10fcc6f4d83dc22842866d9
borealis92 <aurora.fay92@gmail.com>
2018-09-27 21:14:43 -0700

    created hw from 10apps class

---
+++ 10apps/hw.py
@@ -0,0 +1,11 @@
+
+def main():
+    print('-----------------------')
+    print('     This is Barbra')
+    print('-----------------------')
+    name = input("Sorry, couldn't hear that? What was your name: ")
+    print('Nice to meet ya ' + name)
+
+
+
+main()
