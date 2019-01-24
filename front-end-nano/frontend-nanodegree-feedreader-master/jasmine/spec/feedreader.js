caea201a8076d2f46d81f67c2a5fe907b38532d7
Aurora <aurora.fay92@gmail.com>
2019-01-24 08:43:37 -0800

    Added a few more tests

--- front-end-nano/frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
+++ front-end-nano/frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
@@ -68,15 +68,36 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('menu changed when clicked', function(){
+            //Simulate a click
+           var menuIcon = $('.menu-icon-link');
+           menuIcon.click();
+           expect(document.body.classList.contains('menu-hidden')).toBe(false);
+           menuIcon.click();
+           expect(document.body.classList.contains('menu-hidden')).toBe(true);
+        });
+    });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
+     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+        beforeEach(function(done){
+            loadFeed(0,function(){
+                done();
+            });
+        });
+        it('completes its work', function(done){
+            var entries = $('.feed .entry');
+            expect(entries.length).toBeGreaterThan(0);
+            done();
+        })
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
@@ -85,7 +106,6 @@ $(function() {
          * Remember, loadFeed() is asynchronous.
          */
 
-    });
-
+ 
  
 }());
