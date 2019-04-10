775e1b1a500c345ec883c6238070c1daf5342548
Aurora <aurora.fay92@gmail.com>
2019-04-09 20:41:32 -0700

    all unique characters program

---
+++ ctci/1.1_AllUniqueChars/main.py
@@ -0,0 +1,10 @@
+def allUniqueChars(s):
+  for i in range(len(s)):
+    for j in range(i+1, len(s)):
+      if s[i] == s[j]:
+        return False
+  return True      
+
+print(allUniqueChars('Auora'))
+
+
