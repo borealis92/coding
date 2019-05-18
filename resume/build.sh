1ab1f4dfb8f3aa01a7dd69175df0cdf8e7f04d2d
Aurora <aurora.fay92@gmail.com>
2019-05-18 07:07:50 -0700

    Updated resume

--- resume/build.sh
+++
@@ -1,4 +0,0 @@
-name=resume-aurora-siegel
-pdflatex ${name}.tex
-rm -f ${name}.bak ${name}.aux ${name}.log ${name}.out ${name}.nav ${name}.snm ${name}.toc ${name}.bbl ${name}.blg ${name}.lof texput.log *~
-evince ${name}.pdf &
